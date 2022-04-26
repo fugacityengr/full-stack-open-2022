7ea44b95a336552c8ad8296147bf9accd938f96b
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2022-04-26 20:52:00 +0800

    separate module
    courseinformation step10

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,38 +1,4 @@
-const Header = ({ name }) => (
-  <>
-    <h2>{name}</h2>
-  </>
-);
-
-const Parts = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <p key={part.id}>
-        {part.name} {part.exercises}
-      </p>
-    ))}
-  </>
-);
-
-const Total = ({ parts }) => (
-  <>
-    <strong>
-      total of {parts.reduce((total, part) => total + part.exercises, 0)}{" "}
-      exercises
-    </strong>
-  </>
-);
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <h1>Web development curriculum</h1>
-      <Header name={course.name} />
-      <Parts parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
