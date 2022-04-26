f88da239e24820c606ed7885b835d73dc2f1789d
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2022-04-26 20:49:37 +0800

    courseinformation step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = ({ name }) => (
   <>
-    <h1>{name}</h1>
+    <h2>{name}</h2>
   </>
 );
 
@@ -26,6 +26,7 @@ const Total = ({ parts }) => (
 const Course = ({ course }) => {
   return (
     <>
+      <h1>Web development curriculum</h1>
       <Header name={course.name} />
       <Parts parts={course.parts} />
       <Total parts={course.parts} />
@@ -34,28 +35,59 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-  return <Course course={course} />;
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Course course={course} />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default App;
