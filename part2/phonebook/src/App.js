a189a0396f19967e2fef254e0cdd33eac90e24b7
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2022-04-27 21:19:34 +0800

    phonebook step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,23 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterCharacter, setFilterCharacter] = useState("");
 
   const handlePersonInput = (event) => {
     setNewName(event.target.value);
   };
   const handlerNumberInput = (event) => {
     setNewNumber(event.target.value);
+  };
+  const handleFilterInput = (event) => {
+    setFilterCharacter(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -21,6 +28,7 @@ const App = () => {
       return;
     }
     const personObject = {
+      id: persons.length + 1,
       name: newName,
       number: newNumber,
     };
@@ -28,9 +36,21 @@ const App = () => {
     setNewName("");
   };
 
+  const personsToShow =
+    filterCharacter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filterCharacter)
+        );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filterCharacter} onChange={handleFilterInput} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonInput} />
@@ -43,8 +63,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
+      {personsToShow.map((person) => (
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
       ))}
