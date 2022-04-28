f82b5ce6836878ddfaa0d26b8d2a3599a6e14210
fugacityengr <61220814+fugacityengr@users.noreply.github.com>
2022-04-28 19:40:28 +0800

    countries step2

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,30 +1,47 @@
+import { useState } from "react";
+
+const ShowButton = ({ country }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setShowInfo(!showInfo)}>show</button>
+      <div>{showInfo ? <CountryDetail country={country} /> : null}</div>
+    </div>
+  );
+};
+
+const CountryDetail = ({ country }) => (
+  <div>
+    <h2>{country.name.common}</h2>
+    <div>capital {country.capital}</div>
+    <div>area {country.area}</div>
+    <h3>languages:</h3>
+    <ul>
+      {Object.values(country.languages).map((language, index) => (
+        <li key={index}>{language}</li>
+      ))}
+    </ul>
+    <div style={{ fontSize: "10rem" }}>{country.flag}</div>
+  </div>
+);
+
 const Countries = ({ countries, filterCharacter }) => {
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(filterCharacter)
   );
+
   if (filteredCountries.length > 10) {
     return <div>Too many matches, specify another filter</div>;
   } else if (filteredCountries.length === 1) {
     const [country] = filteredCountries;
-    return (
-      <div>
-        <h2>{country.name.common}</h2>
-        <div>capital {country.capital}</div>
-        <div>area {country.area}</div>
-        <h3>languages:</h3>
-        <ul>
-          {Object.values(country.languages).map((language, index) => (
-            <li key={index}>{language}</li>
-          ))}
-        </ul>
-        <div style={{ fontSize: "10rem" }}>{country.flag}</div>
-      </div>
-    );
+    return <CountryDetail country={country} />;
   }
   return (
     <>
-      {filteredCountries.map((country) => (
-        <div key={country.name.common}>{country.name.common}</div>
+      {filteredCountries.map((country, index) => (
+        <div key={index}>
+          <div>{country.name.common}</div> <ShowButton country={country} />
+        </div>
       ))}
     </>
   );
